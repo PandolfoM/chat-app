fd4337b906a2c4f59300b570d02fcd50e37cb053
Matthew Pandolfo <matt@pandolfo.com>
2024-07-11 17:22:47 -0400

    login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,78 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
 import Button from "../components/Button";
 import { Input } from "../components/Input";
+import { auth } from "../firebase";
+import { useContext, useState } from "react";
+import { AuthContext } from "../auth/context";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    email: yup.string().required(),
+    password: yup.string().required(),
+  })
+  .required();
+type FormData = yup.InferType<typeof schema>;
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+  const { setCurrentUser } = useContext(AuthContext);
+  const [error, setError] = useState<string>("");
+
+  const handleLogin = (data: FormData) => {
+    signInWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setCurrentUser(user);
+      })
+      .catch((err) => {
+        const msg: string = err.message;
+        if (msg.includes("invalid-email")) {
+          setError("Invalid email");
+        } else if (msg.includes("invalid-credential")) {
+          setError("Invalid credentials");
+        } else {
+          setError(msg);
+        }
+      });
+  };
+
   return (
-    <div className="w-full h-full flex items-center justify-center flex-col gap-4 p-2">
-      <div className="flex flex-col gap-2 w-full">
-        <Input placeholder="Email" />
-        <Input placeholder="Password" type="password" />
-        <p className="opacity-50 text-sm">
-          Don't have an account?{" "}
-          <Button variant="ghost" className="p-0">
-            Sign up
-          </Button>
-        </p>
+    <form
+      className="flex flex-col gap-2 w-full h-full justify-center p-2"
+      onSubmit={handleSubmit(handleLogin)}>
+      <h1 className="text-center text-xl">Login</h1>
+      <div>
+        <Input placeholder="Email" {...register("email")} />
+        <p className="text-sm text-error">{errors.email?.message}</p>
       </div>
-      <Button className="w-full">Login</Button>
-    </div>
+
+      <div>
+        <Input
+          placeholder="Password"
+          type="password"
+          {...register("password")}
+        />
+        <p className="text-sm text-error">{errors.password?.message}</p>
+      </div>
+
+      <p className="opacity-50 text-sm">
+        Don't have an account?{" "}
+        <a className="p-0 underline" href="/signup">
+          Create an account
+        </a>
+      </p>
+      {error && <p className="text-sm text-error">{error}</p>}
+      <Button className="w-full mt-4" type="submit">
+        Login
+      </Button>
+    </form>
   );
 }
 
